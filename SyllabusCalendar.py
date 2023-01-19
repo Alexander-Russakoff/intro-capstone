dc0dd9b3989f8e1e5c70739d025b8859aa370a9a
Alexander-Russakoff <98143670+Alexander-Russakoff@users.noreply.github.com>
2023-01-18 20:06:39 -0500

    Updated with ability to choose event type

--- SyllabusCalendar.py
+++ SyllabusCalendar.py
@@ -4,16 +4,12 @@ Standalone Syllabus Calendar
 """
 
 
-import json
 import requests 
-import datetime
-import pprint
+
 
 import tkinter as tk
 #from tkinter import ttk
 from tkinter import scrolledtext
-from DateBounder import getSundayAfterNext
-from DateBounder import isValidDay
 from DateBounder import removeEventNotWithinTwoWeeks
 
 
@@ -38,37 +34,75 @@ def makeRequest():
     
     return None
 
-def dateRange():
-    pass
+#update the text based on event type
+def updateText(calendar,events,eventType):
+    calendar.configure(state='normal')
+    calendar.delete(1.0,tk.END)
+    calendar.insert(tk.END,eventsByType(events,eventType))
+    print(eventsByType(events,eventType))
+    calendar.configure(state='disabled')
+
+#return an event of a specific type
+#allowed types: assignment lecture lab sprint break
+def eventsByType(events,eventType):
+    return prettyPrint([x for x in events if x['class_type']==eventType])
+    
 
 def prettyPrint(events):
     DATE_KEY = 'event_date'
     NAME_KEY = 'event_name'
     DESCRIPTION_KEY = 'event_description'
+    TYPE_KEY = 'class_type'
     
     temp = ""
     for x in events:
-        temp = temp + x[DATE_KEY] + " | " + x[NAME_KEY] + "\n\n" + x[DESCRIPTION_KEY]+"\n\n"
+        temp = temp + x[DATE_KEY] + " | " + x[NAME_KEY] +" | "+ x[TYPE_KEY]+ "\n\n" + x[DESCRIPTION_KEY]+"\n\n"
     return temp
 
 print("Begin")
 
-events = makeRequest()
-events = removeEventNotWithinTwoWeeks(events)
+#get all events
+events_all = makeRequest()
+#get all events for this week and the following week
+events = removeEventNotWithinTwoWeeks(events_all)
+
 text = prettyPrint(events)
 
 print(text)
 
+
+
 root = tk.Tk()
 
 root.title("Capstone Calender")
-root.geometry("600x400")
+root.geometry("800x500")
 #labButton = ttk.Button
-calendar = scrolledtext.ScrolledText(root,font={"Arial Bold",20})#,wrap=tk.WORD)
-calendar.insert(tk.INSERT,text)
-#calendar.configure(state="disabled")
-calendar.pack()
+calendar = scrolledtext.ScrolledText(root,font={"Arial Bold",20},width=78,height=24)#,wrap=tk.WORD)
+#calendar.winfo_geometry("800x450")
+buttonFrame = tk.Frame(root)
 
+
+assignmentButton = tk.Button(buttonFrame, text="Assignments", command =lambda: updateText(calendar,events_all,"Assignment"))
+lectureButton = tk.Button(buttonFrame, text="Lectures", command =lambda: updateText(calendar,events_all,"Lecture"))
+labButton = tk.Button(buttonFrame, text="Labs", command =lambda: updateText(calendar,events_all,"Lab"))
+sprintButton = tk.Button(buttonFrame, text="Sprints",command=lambda: updateText(calendar,events_all,"Sprint"))
+breakButton = tk.Button(buttonFrame, text="BREAK", command=lambda: updateText(calendar,events_all,"Break"))
+
+
+calendar.insert(tk.INSERT,text)
+calendar.configure(state="disabled")
+
+calendar.grid(row=0,column=0)
+
+assignmentButton.grid(row=0,column=0,sticky='W')
+lectureButton.grid(row=0,column=1,sticky='W')
+labButton.grid(row=0,column=2,sticky='W')
+sprintButton.grid(row=0,column=3,sticky='W')
+breakButton.grid(row=0,column=4,sticky='W')
+
+buttonFrame.grid(row=1,column=0,sticky='W')
+#calendar.delete(1.0,tk.END)
+#calendar.insert(tk.END, eventsByType(events_all,"Assignment"))
 root.mainloop()
 
 prettyPrint(events)
