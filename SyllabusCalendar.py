0a7928af9cc10e5e04d5973a93536ba4b3bf9306
Alexander-Russakoff <98143670+Alexander-Russakoff@users.noreply.github.com>
2023-01-19 14:01:40 -0500

    updated text formatting

--- SyllabusCalendar.py
+++ SyllabusCalendar.py
@@ -34,12 +34,37 @@ def makeRequest():
     
     return None
 
+def paintFormattedText(calendar,text):
+    calendar.tag_configure('big', font=('monospace', 14, 'bold','underline'))
+    calendar.tag_configure('smoll',font=('monospace',12))
+    for x in range(0,len(text)):
+        if x%2:
+            calendar.insert(tk.END,text[x],'smoll')
+        else:
+            calendar.insert(tk.END,text[x],'big')
+
 #update the text based on event type
 def updateText(calendar,events,eventType):
     calendar.configure(state='normal')
     calendar.delete(1.0,tk.END)
-    calendar.insert(tk.END,eventsByType(events,eventType))
-    print(eventsByType(events,eventType))
+    temp = eventsByType(events,eventType)
+    paintFormattedText(calendar,temp)
+    #calendar.insert(tk.END,eventsByType(events,eventType))
+    #print(eventsByType(events,eventType))
+    calendar.configure(state='disabled')
+
+    
+    
+def upcoming(calendar,text):
+    calendar.configure(state='normal')
+
+    calendar.delete(1.0,tk.END)
+    
+    paintFormattedText(calendar,text)
+               
+    #calendar.delete(1.0,tk.END)
+    #calendar.insert(tk.END,text)
+    #print(eventsByType(events,eventType))
     calendar.configure(state='disabled')
 
 #return an event of a specific type
@@ -47,19 +72,20 @@ def updateText(calendar,events,eventType):
 def eventsByType(events,eventType):
     return prettyPrint([x for x in events if x['class_type']==eventType])
     
-
+#modified to return a list
 def prettyPrint(events):
     DATE_KEY = 'event_date'
     NAME_KEY = 'event_name'
     DESCRIPTION_KEY = 'event_description'
     TYPE_KEY = 'class_type'
     
-    temp = ""
+    temp = []
     for x in events:
-        temp = temp + x[DATE_KEY] + " | " + x[NAME_KEY] +" | "+ x[TYPE_KEY]+ "\n\n" + x[DESCRIPTION_KEY]+"\n\n"
+        temp.append(x[DATE_KEY] + " : " + x[NAME_KEY] +" : "+ x[TYPE_KEY])
+        temp.append("\n\n" + x[DESCRIPTION_KEY]+"\n\n")
     return temp
 
-print("Begin")
+#print("Begin")
 
 #get all events
 events_all = makeRequest()
@@ -68,7 +94,7 @@ events = removeEventNotWithinTwoWeeks(events_all)
 
 text = prettyPrint(events)
 
-print(text)
+#print(text)
 
 
 
@@ -77,32 +103,39 @@ root = tk.Tk()
 root.title("Capstone Calender")
 root.geometry("800x500")
 #labButton = ttk.Button
-calendar = scrolledtext.ScrolledText(root,font={"Arial Bold",20},width=78,height=24)#,wrap=tk.WORD)
+calendar = scrolledtext.ScrolledText(root,font={"Arial Bold",20},width=78,height=24,wrap=tk.WORD)
 #calendar.winfo_geometry("800x450")
 buttonFrame = tk.Frame(root)
 
+upcomingButton = tk.Button(buttonFrame, text="Upcoming", font = ("Helvetica", 15, "bold"), command =lambda: upcoming(calendar,text))
 
-assignmentButton = tk.Button(buttonFrame, text="Assignments", command =lambda: updateText(calendar,events_all,"Assignment"))
-lectureButton = tk.Button(buttonFrame, text="Lectures", command =lambda: updateText(calendar,events_all,"Lecture"))
-labButton = tk.Button(buttonFrame, text="Labs", command =lambda: updateText(calendar,events_all,"Lab"))
-sprintButton = tk.Button(buttonFrame, text="Sprints",command=lambda: updateText(calendar,events_all,"Sprint"))
-breakButton = tk.Button(buttonFrame, text="BREAK", command=lambda: updateText(calendar,events_all,"Break"))
+assignmentButton = tk.Button(buttonFrame, text="Assignments", font = ("Helvetica", 15, "bold"), command =lambda: updateText(calendar,events_all,"Assignment"))
+lectureButton = tk.Button(buttonFrame, text="Lectures", font = ("Helvetica", 15, "bold"), command =lambda: updateText(calendar,events_all,"Lecture"))
+labButton = tk.Button(buttonFrame, text="Labs", font = ("Helvetica", 15, "bold"),  command =lambda: updateText(calendar,events_all,"Lab"))
+sprintButton = tk.Button(buttonFrame, text="Sprints", font = ("Helvetica", 15, "bold"), command=lambda: updateText(calendar,events_all,"Sprint"))
+breakButton = tk.Button(buttonFrame, text="BREAK", font = ("Helvetica", 15, "bold"), command=lambda: updateText(calendar,events_all,"Break"))
 
+#calendar.tag_config("text_highlight", font=("Arial Bold",12))
 
-calendar.insert(tk.INSERT,text)
+#calendar.insert(tk.INSERT,text)
+upcoming(calendar,text)
 calendar.configure(state="disabled")
 
+#calendar.tag_add("text_highlight", "1.0", tk.END)
+
 calendar.grid(row=0,column=0)
+
 
 assignmentButton.grid(row=0,column=0,sticky='W')
 lectureButton.grid(row=0,column=1,sticky='W')
 labButton.grid(row=0,column=2,sticky='W')
 sprintButton.grid(row=0,column=3,sticky='W')
 breakButton.grid(row=0,column=4,sticky='W')
-
+upcomingButton.grid(row=0,column=5,sticky='W')
+#add frame to window
 buttonFrame.grid(row=1,column=0,sticky='W')
 #calendar.delete(1.0,tk.END)
 #calendar.insert(tk.END, eventsByType(events_all,"Assignment"))
 root.mainloop()
 
-prettyPrint(events)
+#prettyPrint(events)
